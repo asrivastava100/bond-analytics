9350432b3e3ffeaf2ca7e66e5fb6756530526f8a
Anuj <anujsr@hotmail.com>
2024-11-01 00:24:37 +0530

    allow for base yield curve and bumps to discount factor

--- Curve.py
+++ Curve.py
@@ -1,7 +1,10 @@
 class Curve():
-    def __init__(self,tenors, rates):
+    def __init__(self, tenors, base_yield_curve):
         self.tenors = tenors
-        self.rates = rates
+        self.base_yield_curve = base_yield_curve
 
-    def get_disc_factor(self):
-        return (1+self.rates)**(-self.tenors)
+    def get_disc_factor(self, spread):
+        return (1 + self.base_yield_curve + spread) ** (-self.tenors)
+    
+    def bump_disc_factor(self, spread, bump):
+        return (1 + self.base_yield_curve + spread + bump) ** (-self.tenors)
